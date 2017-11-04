10ee6b6a4dc7794e59f5994aec1dffdfa3db20d7
gur1preet <gurpreet.pahul@hotmail.com>
2017-11-04 14:17:54 +0530

    Delete method corrected

--- js/main.js
+++ js/main.js
@@ -10,25 +10,21 @@ function submitDetails(){
     passoutYear.push(document.getElementById("formPassoutYear").value);
     stream.push(document.getElementById("formStream").value);
 
-
     let tableBody = document.getElementById("tableBody");
 
     let nodeCheck = document.createElement("input");
-    nodeCheck.id = "checkbox" + index;
     nodeCheck.type = "checkbox";
-    nodeCheck.classname = "checkBox";
+    nodeCheck.classname = "ch";
+    nodeCheck.name = "box"
 
-    let tdSN = document.createElement("td");
     let tdName = document.createElement("td");
     let tdRollNO = document.createElement("td");
     let tdStream = document.createElement("td");
     let tdPassoutYear = document.createElement("td");;
     let tdSelect = document.createElement("td");
-    var tr = document.createElement("tr");
-    tr.classname = "rowData";
+    let tr = document.createElement("tr");
+    tr.ClassName = "tr";
 
-    tdSN.appendChild(document.createTextNode(index+1));
-    tr.appendChild(tdSN);
     tdName.appendChild(document.createTextNode(nameN[index]));
     tr.appendChild(tdName);
     tdRollNO.appendChild(document.createTextNode(rollNo[index]));
@@ -48,14 +44,15 @@ function editDetails(){
 }
 
 function deleteDetails() {
-    let tableBody = document.getElementById("tableBody");
-    let tr = document.getElementsByClassName("rowData");
-    let ch = document.getElementsByClassName("checkBox");
+    let table_Body = document.getElementById("tableBody");
+    let tr = document.getElementsByClassName("tr");
+    let ch = document.getElementsByName('box');
     let count = 0;
 
     for (let x=0;x<ch.length; x++){
+        console.log("in loop");
         if(ch[x].checked){
-            tableBody.removeChild(tr[x],1);
+            table_Body.deleteRow(x);
             nameN.splice(x,1);
             rollNo.splice(x,1);
             passoutYear.splice(x,1);
@@ -63,7 +60,9 @@ function deleteDetails() {
             x=-1;
             count++;
         }
+        a++;
     }
+    index -= count;
 }
 
 function validate() {
